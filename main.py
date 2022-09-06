f53b284f4ff9947bf5c22b01d5359082ae681f74
KernTHeAce <vanatuz2@gmail.com>
2022-09-06 18:01:42 +0300

    feat(server): add socket

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
 from flask import Flask
 from flask import request, jsonify
 from data import OutputJSON
+import json
+from flask_sock import Sock
+
 
 app = Flask(__name__)
+sock = Sock(app)
 
 data = OutputJSON()
+
+messages = []
+
+
+@sock.route('/add')
+def add(sock):
+    while True:
+        data = sock.receive()
+        messages.append(data)
+        sock.send(messages)
+
+@sock.route('/get')
+def get(sock):
+    while True:
+        data = sock.receive()
+        sock.send(messages)
 
 
 @app.route('/')
 def index():
-    return data.to_json()
+    response = jsonify(json.loads(data.to_json()))
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 
 @app.route('/update_Ivan_girls', methods=["POST"])
@@ -20,7 +42,15 @@ def update_ivan_girls():
                    message="Success",
                    statusCode=200), 200
 
+@app.route("/test", methods=["POST"])
+def test():
+    data = request.form.get('data')
+    messages.append(data)
+    return jsonify(isError=False,
+                   message="Success",
+                   statusCode=200), 200
+
 
 if __name__ == '__main__':
     app.debug = True
-    app.run()  # go to http://localhost:5000/ to view the page.
+    app.run(host="192.168.62.12", port=5000)  # go to http://localhost:5000/ to view the page.
